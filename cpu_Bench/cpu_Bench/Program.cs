3731504059cc25b06ac1550ec31cf7758756ce13
Marek Kubička <kubicka.marek@gmail.com>
2016-12-08 12:03:59 +0100

    Update Benchmark PreFinal

--- cpu_Bench/cpu_Bench/Program.cs
+++ cpu_Bench/cpu_Bench/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
+using System.IO;
 
 namespace cpu_Bench
 {
@@ -14,18 +10,37 @@ namespace cpu_Bench
             //--BENCHMARK CPU--//
             long time = DateTime.Now.Ticks;
             long lastTime = time;
-            long nowTime=0;
+            float totalTime = 0f;
+            int i = 0;
+            int totalTicks = 0;
+            float exeTicks = 10000000f;
 
-            while (true)
+            while (totalTicks < exeTicks)
             {
                 time = DateTime.Now.Ticks;
-                nowTime = time - lastTime;
-                Console.Write(nowTime/10000000);
-                Thread.Sleep(100);
-                Console.Clear();
+                totalTime = time - lastTime;
+                if(i%100000==0)
+                {
+                    Console.Clear();
+                    Console.WriteLine(totalTime / 10000000);
+                    i = 0;
+                }          
+                i++;
+                totalTicks++;
             }
-            
-            Console.ReadKey();
+
+            logBenchmarkScore(totalTime, totalTicks, 1);
+            //Console.WriteLine("Completed...");
+            //Console.ReadKey();
+        }
+
+        static void logBenchmarkScore(float ttlTime, int ttlTicks, int core)
+        {
+            float score = ttlTicks / ttlTime;
+            StreamWriter sw = new StreamWriter(core.ToString()+".txt");
+            sw.WriteLine("Core: {0} | Total time: {1} | Total ticks: {2} | Score: {3}",core, ttlTime, ttlTicks, score*100);
+            sw.Flush();
+            sw.Close();
         }
     }
 }
